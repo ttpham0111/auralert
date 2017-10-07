3684f18b0d8c524daf830d6a1bc06cdc6e95ef57
Tuan Pham <tpham@ancestry.com>
2017-10-07 14:07:34 -0700

    Add code to send serial data to server

---
+++ arduino/send_data.py
@@ -0,0 +1,33 @@
+from argparse import ArgumentParser
+
+import requests
+from serial import Serial
+
+
+def get_args():
+    parser = ArgumentParser(description='Arduino to server communication')
+
+    parser.add_argument('file', type=str)
+    parser.add_argument('--port', type=int, default=9600)
+    parser.add_argument('--server-url', type=str, default='http://localhost:3000/sensors/volume')
+
+    return parser.parse_args()
+
+
+def run(filename, port, server_url):
+  arduino = Serial(filename, port)
+  while True:
+    try:
+      volume = int(arduino.readline().strip())
+      print 'Received:', volume
+
+      response = requests.post(server_url, json={'volume': volume})
+      response.raise_for_status()
+      print 'Response:', response
+    except Exception as e:
+      print e
+
+
+if __name__ == '__main__':
+  args = get_args()
+  run(args.file, args.port, args.server_url)
--- server/controllers/sensors.js
+++ server/controllers/sensors.js
@@ -3,10 +3,11 @@ var AlertsService = require('../services/alerts');
 
 exports.listen = function(request, response) {
   var volume = request.body.volume;
+
   AlertsService.respond(volume, function(error) {
     if (error) {
-      return res.status(500).json({ error: error });
+      return response.status(500).json({ error: error });
     }
-    return res.status(204).end();
+    return response.status(204).end();
   });
 };
