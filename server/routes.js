4f0c57e1e6926fb5301793f493d1ad3c15d3e388
Tuan Pham <tpham@ancestry.com>
2017-10-10 19:36:12 -0700

    Add status endpoint

--- server/routes.js
+++ server/routes.js
@@ -7,6 +7,8 @@ var SensorsController = require('./controllers/sensors');
 var AlertsController = require('./controllers/alerts');
 
 
+router.get('/status', function(req, res) { res.json({status: 'OK'}); });
+
 router.post('/sensors/volume', SensorsController.listen);
 
 router.get('/alerts', AlertsController.list);
